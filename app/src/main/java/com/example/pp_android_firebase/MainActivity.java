73e40ea31d541b4ff29aec780b51bbe5336de9e3
shhdq <54862478+shhdq@users.noreply.github.com>
2023-07-28 21:10:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pp_android_firebase/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.pp_android_firebase;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button loginButton = findViewById(R.id.login);
+        Button registerButton = findViewById(R.id.register);
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
